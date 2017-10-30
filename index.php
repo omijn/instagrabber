17c06f10c5149619966da1687988ef17a70fd54a
omijn <abhijit.kashyap07@gmail.com>
2017-10-29 17:05:10 -0700

    removed unnecessary elements

--- index.php
+++ index.php
@@ -26,15 +26,8 @@
 
   <body>   
     <div class="container">
-      <div class="header clearfix">
-        <nav>
-          <ul class="nav nav-pills pull-right">
-            <li role="presentation"><a href="#">Home</a></li>
-            <li role="presentation"><a href="#">About</a></li>
-            <li role="presentation"><a href="#">Contact</a></li>
-          </ul>
-        </nav>
-        <h3 class="text-muted">Instagram image downloader</h3>
+      <div class="header clearfix">        
+        <h3>Instagram image downloader</h3>
       </div>
 
 
